2245459e85300e10440eee2ea9b789d701df72a4
ReCore <kjshore2004@gmail.com>
2024-05-10 01:23:41 +0930

    Now as a website. Yay

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,4 +1,4 @@
 export interface LoginRequest { email: string, password: string, }
 export interface Message { author: string, content: string, timestamp: string, channel: string, }
-export interface StatusResponse { db: boolean, }
+export interface StatusResponse { db: boolean, cpu: number, mem: number, redis: boolean, tornado: boolean, }
 export interface User { username: string, password: string, uuid: string, pfp: string, email: string, bio: string, }
